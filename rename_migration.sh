19951ca6ae6b749361dd1211c4fe03777e831d79
Andreas Baldeau <andreas.baldeau@freiheit.com>
2013-12-06 23:19:51 +0100

    Fix for migration script allowing dashes in beginning of hostnames.

--- rename_migration.sh
+++ rename_migration.sh
@@ -16,10 +16,10 @@ cd $PEERS_DIR || fail "Could not cd to $PEERS_DIR"
 
 for file in *; do
     if [ -f $file ]; then
-        host=$(grep "^# Knotenname:" $file | cut -d ' ' -f 3)
-        mac=$(grep "^# MAC:" $file | cut -d ' ' -f 3)
-        key=$(grep "^key " $file | cut -d '"' -f 2)
-        mv $file "$host@$mac@$key"
+        host=$(grep "^# Knotenname:" -- "$file" | cut -d ' ' -f 3)
+        mac=$(grep "^# MAC:" -- "$file" | cut -d ' ' -f 3)
+        key=$(grep "^key " -- "$file" | cut -d '"' -f 2)
+        mv -- "$file" "$host@$mac@$key"
     fi
 done
 
